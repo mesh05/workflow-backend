0ae6e0b58fd227568c73273e704935012cdeb6e2
mesh <mesh@gmail.com>
2024-10-14 22:15:34 +0530

    Fixed env variable issue where it was returning undefined

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,10 +1,11 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
+import dotenv from "dotenv";
 
-const queryClient = postgres(
-  "postgresql://superchatdb_owner:jbeY5UGraf3Z@ep-withered-flower-a1xwpakq.ap-southeast-1.aws.neon.tech/superchatdb?sslmode=require",
-);
+dotenv.config({ path: __dirname + "/../../.env" });
+
+const queryClient = postgres(process.env.DATABASE_URL as string);
 
 const db = drizzle(queryClient, { schema, logger: true });
 
